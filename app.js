6351b0d942672e3a3743c74618a5c9860c2f9d11
hideonbush3 <ppoii0961@gmail.com>
2023-02-17 17:57:15 +0900

    주석 수정

--- app.js
+++ app.js
@@ -1,41 +1,32 @@
 // express 모듈과 기타 미들웨어 모듈 사용 선언
-const express = require("express");
-const path = require("path");
-const logger = require("morgan");
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const { engine } = require('express-handlebars');
+const bodyParser = require('body-parser');
+const oracledb = require('./models/Oracle');
 
 // 라우팅 모듈 설정
 const indexRouter = require("./routes/index");
 const memberRouter = require("./routes/member");
 const boardRouter = require("./routes/board");
-const bodyParser = require("body-parser"); // 폼 처리기
-const oracledb = require("./models/Oracle");
 
-
-// 포트변수 선언 및 express 객체 생성
-const port = process.env.PORT || 3001;
+// express 객체 생성 및 포트 변수 선언
 const app = express();
-
-const { engine } = require("express-handlebars");
-
-app.use(express.static(path.join(__dirname, "static")));
+const port = process.env.PORT || 3001;
 
 // 뷰 엔진 등록
 // 첫번째 인자는 뷰 엔진의 이름
 // 두번째 인자는 handlebars를 구성하는 옵션을 포함하는 함수
-app.engine(
-  "hbs",
-  engine({
-    extname: ".hbs",    // 확장자로 .hbs 사용
-    defaultLayout: "layout",    // 기본 레이아웃 템플릿 지정
-    helpers: {  // handlebars 템플릿에서 사용할 수 있는 함수를 정의하는 객체
-      section: function (name, options) { // section이라는 함수를 정의
-        if (!this._sections) this._sections = {};   // 인자로 섹션이름과 옵션객체를 받음
-        this._sections[name] = options.fn(this);
-        return null;
-      },
-    },
-  })
-);
+app.engine('hbs', engine({
+    extname: '.hbs', defaultLayout: 'layout',
+    helpers: {
+        section: function(name, options) {
+            if(!this._sections) this._sections = {}
+            this._sections[name] = options.fn(this)
+            return null
+        }, },
+}));
 
 // 렌더링할 view 파일들의 위치로 'views' 폴더를 지정
 app.set("views", path.join(__dirname, "views"));
@@ -46,15 +37,20 @@ app.set("views", path.join(__dirname, "views"));
 // 즉, app.engine 에서는 엔진을 등록하고 여기선 사용한다고 선언하는 것
 app.set("view engine", "hbs");
 
+app.use(express.static(path.join(__dirname, "static")));
+
 app.use(logger("dev"));
 
 // 미들웨어 등록 및 설정
 app.use(express.json());
 // 전송된 폼 데이터에 대한 urlencoding 설정
 app.use(express.urlencoded({ extended: false }));
-app.use(bodyParser.json()); // 전송된 폼 데이터는 json 형식
+// 전송된 폼 데이터는 json 형식
+app.use(bodyParser.json());
 // app.use(bodyParser.text()) // enctype이 text/plain 일때 필요 (비추)
-oracledb.initConn(); // 오라클 instant client 초기화
+
+// 오라클 instant client 초기화
+oracledb.initConn();
 
 // 라우팅 모듈 등록 - 클라이언트 요청 처리 핵심 파트
 app.use("/", indexRouter);
@@ -69,7 +65,7 @@ app.use((req, res) => {
 app.use((err, req, res, next) => {
   console.log(err);
   res.status(500);
-  res.send("500-서버 내부 오류발생!");
+  res.send("500-서버 내부 오류발생");
 });
 
 // 위에서 설정한 사항을 토대로 express 서버 실행
