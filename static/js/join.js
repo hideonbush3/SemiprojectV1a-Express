9b72c22594334fa62edea87b729efe13064ac9e2
hideonbush3 <ppoii0961@naver.com>
2023-02-20 12:12:19 +0900

    주석 수정

--- static/js/join.js
+++ static/js/join.js
@@ -1,10 +1,10 @@
 const processJoin = () => {
     let frm = document.join
-    if(frm.userid.value === '') alert('아이디는?')
-    else if(frm.passwd.value === '') alert('비밀번호는?')
-    else if(frm.repwd.value === '') alert('비밀번호는?')
-    else if(frm.name.value === '') alert('이름은?')
-    else if(frm.email.value === '') alert('이메일은?')
+    if(frm.uid.value === '') alert('아이디를 입력하세요')
+    else if(frm.pwd.value === '') alert('비밀번호를 입력하세요')
+    else if(frm.repwd.value === '') alert('비밀번호를 입력하세요')
+    else if(frm.name.value === '') alert('이름을 입력하세요')
+    else if(frm.email.value === '') alert('이메일을 입력하세요')
     else{
         frm.method= 'post';
         frm.enctype = 'application/x-www-form-urlencoded';
