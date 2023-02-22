de361340befd47db5c54df47b8c06750a656ff44
hideonbush3 <ppoii0961@gmail.com>
2023-02-23 00:28:57 +0900

    우편번호 검색앱2 - 동, 우편번호 검색, null을 공백으로 변환

--- routes/zipcode2.js
+++ routes/zipcode2.js
@@ -1,4 +1,4 @@
-// 클라이언트 사이드 렌더링은 SSR에 비해 코드가 간단함
+// CCR은 SSR에 비해 코드가 간단함
 const express = require("express");
 const router = express.Router();
 const Zipcode = require("../models/Zipcode");
@@ -9,9 +9,13 @@ router.get("/", async (req, res) => {
 router.get("/sido", async (req, res) => {
   let sidos = new Zipcode().getSido().then((sido) => sido);
 
-  res.send(JSON.stringify(await sidos));  // 조회결과를 JSON 형식으로 전송
+  res.send(JSON.stringify(await sidos));  // 조회결과를 JSON 문자열 형태로 전송
 });
 
+// path variable
+// REST API에서 사용하는 방식으로 경로를 변수처럼 사용하는 기법
+// express 프레임워크에서 /:변수명 형식으로 사용하고
+// 변수의 값을 가져오려면 req.params.변수명
 router.get("/gugun/:sido", async (req, res) => {
   // url로 요청한 sido값을 가져옴
   let sido = req.params.sido;
@@ -19,8 +23,25 @@ router.get("/gugun/:sido", async (req, res) => {
   // sido값을 파라미터로 넣어서 실행하고 그 결과를 guguns 변수에 저장
   let guguns = new Zipcode().getGugun(sido).then((gugun) => gugun);
 
-  // guguns 변수에 저장된 select 조회결과를 JSON 문자열 형식으로 바꾸고 요청주소로 보내줌
+  // guguns 변수에 저장된 select 조회결과를 JSON 문자열로 변환한 후 HTTP 응답으로 클라이언트에게 보냄
   res.send(JSON.stringify(await guguns));
+});
+
+router.get("/dong/:sido/:gugun", async (req, res) => {
+  let sido = req.params.sido;
+  let gugun = req.params.gugun;
+  let dongs = new Zipcode().getDong(sido, gugun).then((dong) => dong);
+
+  res.send(JSON.stringify(await dongs));
+});
+
+router.get("/zipcode/:sido/:gugun/:dong", async (req, res) => {
+  let sido = req.params.sido;
+  let gugun = req.params.gugun;
+  let dong = req.params.dong;
+  let zips = new Zipcode().getZipcode(sido, gugun, dong).then((zip) => zip);
+
+  res.send(JSON.stringify(await zips));
 });
 
 module.exports = router;
--- static/js/zipcode2.js
+++ static/js/zipcode2.js
@@ -4,12 +4,21 @@ const makeopt = (elm, text) => {
   opt.appendChild(txt);
   elm.appendChild(opt);
 };
-const setSido = (sidos) => {
-  let objs = JSON.parse(sidos)   // 문자열 sidos를 파싱해서 객체로 바꿈
 
-    objs.forEach((obj, idx) => {    // forEach 함수가 objs 객체에 있는 값들을 하나씩 순회하면서 값과 인덱스를 가져옴 그 값을 파라미터로 갖는 콜백함수 실행
-        makeopt(sido, obj.sido);    // makeopt 함수에 obj의 sido값 중 첫번째 값을 파라미터로 넣어줌
-    });
+const makeAddr = (elm, text) => {
+  let p = document.createElement("p");
+  let txt = document.createTextNode(text);
+  p.appendChild(txt);
+  elm.appendChild(p);
+};
+
+const setSido = (sidos) => {
+  let objs = JSON.parse(sidos); // 문자열 sidos를 파싱해서 객체로 바꿈
+
+  objs.forEach((obj, idx) => {
+    // forEach 함수가 objs 객체에 있는 값들을 하나씩 순회하면서 값과 인덱스를 가져옴 그 값을 파라미터로 갖는 콜백함수 실행
+    makeopt(sido, obj.sido); // makeopt 함수에 obj의 sido값 중 첫번째 값을 파라미터로 넣어줌
+  });
 };
 
 // 서버에 시도 데이터 요청
@@ -20,30 +29,66 @@ const getSido = () => {
 };
 
 const setGugun = (guguns) => {
-  let objs = JSON.parse(guguns)
+  let objs = JSON.parse(guguns);
 
-  while(gugun.lastChild){
+  while (gugun.lastChild) {
     gugun.removeChild(gugun.lastChild);
   }
-  makeopt(gugun, '--- 시군구 ---');
+  makeopt(gugun, "--- 시군구 ---");
   objs.forEach((obj, idx) => {
     makeopt(gugun, obj.gugun);
   });
 };
 
 const getGugun = () => {
-  fetch("zipcode2/gugun/" + sido.value)
-      .then((response) => response.text())
-      .then((text) => setGugun(text));
+  fetch(`zipcode2/gugun/${sido.value}`)
+    .then((response) => response.text())
+    .then((text) => setGugun(text));
 };
 
-const getDong = () => {};
+const setDong = (dongs) => {
+  let objs = JSON.parse(dongs);
 
-const getZipcode = () => {};
+  while (dong.lastChild) {
+    dong.removeChild(dong.lastChild);
+  }
+  makeopt(dong, "--- 읍면동 ---");
+  objs.forEach((obj, idx) => {
+    makeopt(dong, obj.dong);
+  });
+};
+
+const getDong = () => {
+  fetch(`zipcode2/dong/${sido.value}/${gugun.value}`)
+    .then((response) => response.text())
+    .then((text) => setDong(text));
+};
+
+const setZipcode = (zips) => {
+  let objs = JSON.parse(zips);
+
+  while (zipcode.lastChild) {
+    zipcode.removeChild(zipcode.lastChild);
+  }
+
+  objs.forEach((obj, idx) => {
+    let addr =
+      `${obj.zipcode} ${obj.sido} ${obj.gugun}` +
+      `${obj.dong} ${obj.ri} ${obj.bunji}`;
+    makeAddr(zipcode, addr);
+  });
+};
+
+const getZipcode = () => {
+  fetch(`zipcode2/zipcode/${sido.value}/${gugun.value}/${dong.value}`)
+    .then((response) => response.text())
+    .then((text) => setZipcode(text));
+};
 
 let sido = document.querySelector("#sido");
 let gugun = document.querySelector("#gugun");
 let dong = document.querySelector("#dong");
+let zipcode = document.querySelector("#zipcode");
 sido.addEventListener("change", getGugun);
 gugun.addEventListener("change", getDong);
 dong.addEventListener("change", getZipcode);
