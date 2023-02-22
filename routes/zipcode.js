798747510d7d1f02a6f0ba4503b23ea374c960d7
hideonbush3 <ppoii0961@gmail.com>
2023-02-22 14:50:14 +0900

    시도, 구군, 읍면동 출력3 - 시도 : 시군구 : 동

--- routes/zipcode.js
+++ routes/zipcode.js
@@ -1,29 +1,36 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Zipcode = require('../models/Zipcode');
+const Zipcode = require("../models/Zipcode");
 
-router.get('/', async (req, res) => {
-    let sido = req.query.sido;
-    let gugun = req.query.gugun;
-    let [guguns, dongs, zips] = [null, null, null];
+router.get("/", async (req, res) => {
+  let sido = req.query.sido;
+  let gugun = req.query.gugun;
+  let dong = req.query.dong;
+  let [guguns, dongs, zips] = [null, null, null];
 
-    let sidos = new Zipcode().getSido().then(sido => sido);
-    // console.log(await sidos);
+  let sidos = new Zipcode().getSido().then((sido) => sido);
+  // console.log(await sidos);
 
-    if(sido !== undefined)
-    guguns = new Zipcode().getGugun(sido).then(gugun => gugun);
-    // console.log(await guguns);
+  if (sido !== undefined)
+    guguns = new Zipcode().getGugun(sido).then((gugun) => gugun);
+  // console.log(await guguns);
 
-    if(sido !== undefined && gugun !== undefined)
-    dongs = new Zipcode().getDong(sido, gugun).then(dong => dong);
-    // console.log(await dongs);
+  if (sido !== undefined && gugun !== undefined)
+    dongs = new Zipcode().getDong(sido, gugun).then((dong) => dong);
+  // console.log(await dongs);
 
-    // let zipcodes = new Zipcode().getZipcode('부산', '수영구', '광안동').then(zipcode => zipcode);
-    // console.log(await zipcodes);
+  if (sido !== undefined && gugun !== undefined && dong !== undefined)
+    zips = new Zipcode().getZipcode(sido, gugun, dong).then((zip) => zip);
 
-    res.render('zipcode', {title: '시군구동 찾기',
-    sidos: await sidos, guguns : await guguns, dongs : await dongs,
-    sido: sido, gugun: gugun});
-})
+  res.render("zipcode", {
+    title: "시군구동 찾기",
+    sidos: await sidos,
+    guguns: await guguns,
+    dongs: await dongs,
+    zips: await zips,
+    sido: sido,
+    gugun: gugun,
+  });
+});
 
 module.exports = router;
