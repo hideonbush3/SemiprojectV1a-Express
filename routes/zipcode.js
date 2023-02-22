fcabc642621db85ef4a7f4edf0b0cea13e4c2f04
hideonbush3 <ppoii0961@naver.com>
2023-02-22 13:11:58 +0900

    시도, 구군, 읍면동 출력2 - 시도 : 시군구

--- routes/zipcode.js
+++ routes/zipcode.js
@@ -3,20 +3,27 @@ const router = express.Router();
 const Zipcode = require('../models/Zipcode');
 
 router.get('/', async (req, res) => {
+    let sido = req.query.sido;
+    let gugun = req.query.gugun;
+    let [guguns, dongs, zips] = [null, null, null];
+
     let sidos = new Zipcode().getSido().then(sido => sido);
-    console.log(await sidos);
+    // console.log(await sidos);
 
-    let guguns = new Zipcode().getGugun('부산').then(gugun => gugun);
-    console.log(await guguns);
+    if(sido !== undefined)
+    guguns = new Zipcode().getGugun(sido).then(gugun => gugun);
+    // console.log(await guguns);
 
-    let dongs = new Zipcode().getDong('부산', '수영구').then(dong => dong);
-    console.log(await dongs);
+    if(sido !== undefined && gugun !== undefined)
+    dongs = new Zipcode().getDong(sido, gugun).then(dong => dong);
+    // console.log(await dongs);
 
-    let zipcodes = new Zipcode().getZipcode('부산', '수영구', '광안동').then(zipcode => zipcode);
-    console.log(await zipcodes);
+    // let zipcodes = new Zipcode().getZipcode('부산', '수영구', '광안동').then(zipcode => zipcode);
+    // console.log(await zipcodes);
 
     res.render('zipcode', {title: '시군구동 찾기',
-    sidos: await sidos, guguns : await guguns, dongs : await dongs});
+    sidos: await sidos, guguns : await guguns, dongs : await dongs,
+    sido: sido, gugun: gugun});
 })
 
 module.exports = router;
