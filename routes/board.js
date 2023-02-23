cd4a30a42d5479252ec224c35c0f9e7ccc71cbeb
hideonbush3 <ppoii0961@naver.com>
2023-02-23 22:44:09 +0900

    게시판 페이징 구현1

--- routes/board.js
+++ routes/board.js
@@ -1,11 +1,47 @@
 const express = require("express");
 const router = express.Router();
 const Board = require("../models/Board");
+const ppg = 15;
 
-router.get("/list", async (req, res) => {
-  let bds = new Board().select().then((bds) => bds);
-  // console.log(await bds)
-  res.render("board/list", { title: "게시판", bds: await bds }); // 템플릿 페이지에서 {{#bds}} 라고 쓰면됨
+// 페이징 기능 지원
+// 현재 페이지를 의미하는 변수 : cpg
+// 현재 페이지에 해당하는 게시물들을 조회하려면 해당범위의 시작값과 종료값 계산
+// cpg : 1 => 1 ~ 5
+// cpg : 2 => 6 ~ 10
+// cpg : 3 => 11 ~ 15
+// 페이지당 게시물 수 ppg : number
+// stnum : (cpg - 1) * ppg + 1
+// ednum : stnum + ppg
+
+router.get('/list', async (req, res) => {
+  let { cpg } = req.query;
+  cpg = cpg ? cpg : 1;
+  let stnum = (cpg - 1) * ppg + 1; // 지정한 페이지의 첫번째 글번호 계산
+  let stpgn = parseInt((cpg - 1) / 10) * 10 + 1; // 페이지네이션 시작값 계산
+
+  // 페이지네이션 블럭 생성
+  let stpgns = [];
+  for (let i = stpgn; i < stpgn + 10; ++i){
+    let iscpg = (i == cpg) ? true : false;  // 현재페이지 표시
+    let pgn = { 'num': i, 'iscpg': iscpg };
+    stpgns.push(pgn);
+  }
+  let alpg = new Board().selectCount().then((cnt) => cnt);  // 총 게시물 수
+
+  let isprev = (cpg - 1 > 0) ? true: false;
+  let isnext = (cpg < alpg) ? true: false;
+  let pgn = {'prev': stpgn - 1, 'next': stpgn + 9 + 1,
+    'isprev': isprev, 'isnext': isnext};
+
+  let bds = new Board().select(stnum).then((bds) => bds);
+  console.log(cpg, stnum, stpgn);
+
+  res.render("board/list", {
+    title: "게시판",
+    bds: await bds, // 템플릿 페이지에서 {{#bds}} 라고 쓰면됨
+    stpgns: stpgns,
+    pgn: pgn,
+  });
 });
 
 router.get("/write", (req, res) => {
@@ -18,8 +54,8 @@ router.post("/write", async (req, res) => {
   let { title, uid, contents } = req.body;
 
   let rowcnt = new Board(null, title, uid, null, contents, null)
-    .insert()
-    .then((result) => result);
+      .insert()
+      .then((result) => result);
 
   if ((await rowcnt) > 0) viewName = "/board/list";
 
@@ -70,9 +106,8 @@ router.post("/update", async (req, res) => {
   let bno = req.query.bno;
   let suid = req.session.userid;
 
-  if (uid && suid && (uid == suid)) {
-    new Board(bno, title, uid, 0, contents, 0)
-        .updete(bno).then((cnt) => cnt);
+  if (uid && suid && uid == suid) {
+    new Board(bno, title, uid, 0, contents, 0).updete(bno).then((cnt) => cnt);
     res.redirect(303, `/board/view?bno=${bno}`);
   } else res.redirect(303, "/board/list");
 });
